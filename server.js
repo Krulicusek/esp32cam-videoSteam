0836b93226bb5a3bfb30ad82f33c452332410a22
Jakub Węgrzynowski <kubawegrzynowski@gmail.com>
2022-03-19 21:14:33 +0100

    login and recconect

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const path = require('path');
-
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
-var compare = require('tsscmp');
 var auth = require('basic-auth')
 const WebSocket = require("ws");
+
 let server = require('http').createServer(function (req, res) {
-    var credentials = auth(req)
-    
+    var credentials = auth(req)    
     if (!credentials || credentials.name !== 'admin' || credentials.pass !== 'secret') {
      res.statusCode = 401
      res.setHeader('WWW-Authenticate', 'Basic realm="example"')
      res.end('Access denied')
     } else {
-     res.end('Acces granted');
+        
     }
    });
-//var reconnect = require('reconnect-ws');
+
 var WSServer = WebSocket.Server;
 var wss = new WSServer({
     server: server,
@@ -26,9 +23,10 @@ var wss = new WSServer({
 
 
 const HTTP_PORT = 8000;
-app.use(bodyParser);
+
 
 app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')));
+
 server.on('request', app);
 let connectedClients = [];
 wss.on('connection', (ws, req) => {
