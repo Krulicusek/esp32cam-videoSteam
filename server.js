e068607f65dfdbf424319f84f5b6710089a62b51
Jakub Węgrzynowski <kubawegrzynowski@gmail.com>
2022-03-19 12:13:28 +0100

    still trying recconnecting ws

--- server.js
+++ server.js
@@ -4,16 +4,21 @@ const passport = require('passport');
 const express = require('express');
 const session = require('express-session');
 const app = express();
-const WebSocket = require('ws');
+const WebSocket = require("ws");//= require("./RecconectingWebSocket");
 let server = require('http').createServer();
 const login = require("./login_routes");
-let WSServer = WebSocket.Server;
-let wss = new WSServer({
+var reconnect = require('reconnect-ws');
+var WSServer = WebSocket.Server;
+var wss = new WSServer({
     server: server,
-    perMessageDeflate: false
-});
+    perMessageDeflate: false    
+    });
+    
 app.use(require('cookie-parser')());
 var bodyParser = require('body-parser');
+const { inspect } = require('util');
+
+wss.rec
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(flash);
 app.use(session({secret: 'secretDogCam',
@@ -29,7 +34,6 @@ app.use("/", login);
 app.get('/client', (req, res) => res.sendFile(path.resolve(__dirname, './client.html')));
 server.on('request', app);
 let connectedClients = [];
-var reconnectInterval = 1000 * 5;
 wss.on('connection', (ws, req) => {
     console.log('Connected');
     connectedClients.push(ws);
@@ -47,8 +51,7 @@ wss.on('connection', (ws, req) => {
         console.log('socket error');
     });
     ws.on('close', function() {
-        console.log('socket close');
-        setTimeout(connect, reconnectInterval);
+        console.log('socket close');      
     });
 });
 
